cd4ac87f6533b802378b65c152d6d1fef0b45805
ritesh <riteshf7@gmail.com>
2025-10-18 01:53:05 +0530

    Fix React linting error: escape apostrophe in PWAStatus component

--- src/components/PWAStatus.tsx
+++ src/components/PWAStatus.tsx
@@ -31,7 +31,7 @@ export default function PWAStatus() {
     <div className="fixed top-0 left-0 right-0 z-50 bg-red-600 text-white py-2 px-4 text-center text-sm">
       <div className="flex items-center justify-center space-x-2">
         <WifiOff className="w-4 h-4" />
-        <span>You're offline. Some features may be limited.</span>
+        <span>You&apos;re offline. Some features may be limited.</span>
       </div>
     </div>
   );
